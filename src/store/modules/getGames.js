a1c07ca78e77fc92d42b7cead24b5df59c1b8bd3
David Liu <48995019+dliu27@users.noreply.github.com>
2023-05-10 22:17:54 -0400

    Update getGames.js

--- src/store/modules/getGames.js
+++ src/store/modules/getGames.js
@@ -4,10 +4,9 @@
 
 import axios from "axios";
 
-import shouldUseCache from "../../helpers/shouldUseCache.js";
+import { shouldUseCache } from "../../helpers/shouldUseCache.js";
 
 const CLIENT_ID = import.meta.env.VITE_APP_TWITCH_APP_CLIENT_ID;
-const ACCESS_TOKEN = import.meta.env.VITE_APP_TWITCH_APP_ACCESS_TOKEN;
 
 const getGames = {
   namespaced: true,
@@ -31,8 +30,9 @@ const getGames = {
   },
 
   actions: {
-    async fetchGames({ commit, getters }, id) {
+    async fetchGames({ commit, getters, rootGetters }, id) {
       const cachedData = getters.getGamesById(id);
+      const ACCESS_TOKEN = rootGetters["getTokenModule/getToken"]().token;
 
       // Use cache, and return
       if (shouldUseCache(cachedData)) {
