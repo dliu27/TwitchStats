0b7b3e3810916bdb0282c4bb9c76bacae5fb7d64
David Liu <48995019+dliu27@users.noreply.github.com>
2023-05-10 22:17:13 -0400

    Update getChannel.js

--- src/store/modules/getChannel.js
+++ src/store/modules/getChannel.js
@@ -4,10 +4,9 @@
 
 import axios from "axios";
 
-import shouldUseCache from "../../helpers/shouldUseCache.js";
+import { shouldUseCache } from "../../helpers/shouldUseCache.js";
 
 const CLIENT_ID = import.meta.env.VITE_APP_TWITCH_APP_CLIENT_ID;
-const ACCESS_TOKEN = import.meta.env.VITE_APP_TWITCH_APP_ACCESS_TOKEN;
 
 const getChannel = {
   namespaced: true,
@@ -31,8 +30,9 @@ const getChannel = {
   },
 
   actions: {
-    async fetchChannel({ commit, getters }, id) {
+    async fetchChannel({ commit, getters, rootGetters }, id) {
       const cachedData = getters.getChannelById(id);
+      const ACCESS_TOKEN = rootGetters["getTokenModule/getToken"]().token;
 
       // Use cache, and return
       if (shouldUseCache(cachedData)) {
