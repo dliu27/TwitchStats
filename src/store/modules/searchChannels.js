b802c8b5e59ade512cc186e35c7d3fe2d4cc4907
David Liu <48995019+dliu27@users.noreply.github.com>
2023-05-10 22:18:58 -0400

    Update searchChannels.js

--- src/store/modules/searchChannels.js
+++ src/store/modules/searchChannels.js
@@ -4,7 +4,7 @@
 
 import axios from "axios";
 
-import shouldUseCache from "../../helpers/shouldUseCache.js";
+import { shouldUseCache } from "../../helpers/shouldUseCache.js";
 
 const CLIENT_ID = import.meta.env.VITE_APP_TWITCH_APP_CLIENT_ID;
 const ACCESS_TOKEN = import.meta.env.VITE_APP_TWITCH_APP_ACCESS_TOKEN;
@@ -31,8 +31,9 @@ const searchChannels = {
   },
 
   actions: {
-    async fetchSearchChannels({ commit, getters }, searchQuery) {
+    async fetchSearchChannels({ commit, getters, rootGetters }, searchQuery) {
       const cachedData = getters.getSearchChannels(searchQuery);
+      const ACCESS_TOKEN = rootGetters["getTokenModule/getToken"]().token;
 
       // Use cache, and return
       if (shouldUseCache(cachedData)) {
