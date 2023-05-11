f41d0bf19a6a2d81bc91b8cc450ce47400aa8e40
David Liu <48995019+dliu27@users.noreply.github.com>
2023-05-10 22:18:49 -0400

    Update getVideos.js

--- src/store/modules/getVideos.js
+++ src/store/modules/getVideos.js
@@ -4,10 +4,9 @@
 
 import axios from "axios";
 
-import shouldUseCache from "../../helpers/shouldUseCache.js";
+import { shouldUseCache } from "../../helpers/shouldUseCache.js";
 
 const CLIENT_ID = import.meta.env.VITE_APP_TWITCH_APP_CLIENT_ID;
-const ACCESS_TOKEN = import.meta.env.VITE_APP_TWITCH_APP_ACCESS_TOKEN;
 
 const getVideos = {
   namespaced: true,
@@ -31,8 +30,9 @@ const getVideos = {
   },
 
   actions: {
-    async fetchVideos({ commit, getters }, id) {
+    async fetchVideos({ commit, getters, rootGetters }, id) {
       const cachedData = getters.getVideosById(id);
+      const ACCESS_TOKEN = rootGetters["getTokenModule/getToken"]().token;
 
       // Use cache, and return
       if (shouldUseCache(cachedData)) {
